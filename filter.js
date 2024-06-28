f309166f26796d3b409fa1002fafabd764a82057
James King <jamesking.1711@gmail.com>
2024-06-28 08:43:14 +0000

    Added filter tutorial.

--- filter.js
+++ filter.js
@@ -21,6 +21,12 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === "Paul")[0];
+console.log(paul);
+
 
 // Complex Filtering
 const students = [
@@ -55,3 +61,9 @@ const students = [
     ]
   },
 ];
+
+const has5YrsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5YrsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+const names = candidates.map(candidate => candidate.name);
+console.log(names);
